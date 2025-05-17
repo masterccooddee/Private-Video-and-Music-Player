cbc2bc9f3cd9274ba2973b21f95f869f4a2d35cb
DeemoEvan <evan930212@gmail.com>
2025-05-17 17:50:44 +0800

    video

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,15 +1,16 @@
 import React, { useEffect, useRef, useState } from 'react';
 import videojs from 'video.js';
 import 'video.js/dist/video-js.css';
-import { MediaPlayer } from 'dashjs';
+
 
 const VideoPlayer = () => {
   const videoRef = useRef(null);
   const dashPlayerRef = useRef(null);
 
-  const [src, setSrc] = useState('https://dash.akamaized.net/envivio/EnvivioDash3/manifest.mpd');
-  const [poster, setPoster] = useState('https://via.placeholder.com/640x360');
-  const [subtitles, setSubtitles] = useState('./example.vtt'); // 字幕文件的 URL
+  const [loading, setLoading] = useState(true)
+  const [src, setSrc] = useState(null);
+  const [poster, setPoster] = useState(null);
+  const [subtitles, setSubtitles] = useState(null); // 字幕文件的 URL
 
   useEffect(() => {
     const videoElement = videoRef.current;
@@ -21,25 +22,24 @@ const VideoPlayer = () => {
         const player = videojs(videoElement, {
           controls: true,
           autoplay: false,
-          preload: 'auto',
-          poster,
         });
-
-        // 添加字幕 track
+        fetch('http://localhost:3000/video/video:1')
+        .then(res => {return res.json()})
+        .then(data => {
+          setSrc(data.video_url);
+          setPoster(data.poster_url);
+          setSubtitles(data.subtitles_url); // 設定字幕文件的 URL
+          setLoading(false);
+        });
+        /* // 添加字幕 track
         videoElement.innerHTML += `
           <track kind="subtitles" src="${subtitles}" srclang="en" label="English" default>
-        `;
-
+        `; */
         // 初始化 dash.js 播放器
-        const dashPlayer = MediaPlayer().create();
-        dashPlayer.initialize(videoElement, src, false);
-        dashPlayerRef.current = dashPlayer;
-
         return () => {
           player.dispose();
-          dashPlayer.reset();
         };
-      }, 100);
+      }, 10);
     }
   }, [src, poster, subtitles]);  // 監聽字幕、視頻源或海報的變化
 
@@ -51,15 +51,8 @@ const VideoPlayer = () => {
 
   return (
     <div>
-      <video
-        ref={videoRef}
-        className="video-js vjs-default-skin"
-        controls
-        style={{ width: '200%', height: 'auto', maxWidth: '640px' }}
-      >
-        {/* 字幕軌道 */}
-        <track kind="subtitles" src={subtitles} srclang="en" label="English" default />
-      </video>
+      {loading && <div>載入中...</div>}
+      <video ref ={videoRef}  id="my-video"  className="video-js vjs-default-skin" controls  width="1280" height="720"></video>
 
       {/* 切換視頻按鈕 */}
       <div style={{ marginTop: '400px', textAlign: 'center' }}>
