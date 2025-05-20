61d3a4effa0f69288bd633dcca90128d20ae9668
ddww0505 <wangys0505@gmail.com>
2025-05-20 21:43:36 +0800

    chore:加一個server放在terminal的腳本

---
+++ server_start_quick.bat
@@ -0,0 +1,10 @@
+@echo off
+call npm install
+call npm run build 
+xcopy .\dist\*.* .\public /E /Y 
+if not exist ".\public\icons" mkdir ".\public\icons"
+xcopy .\icons .\public\icons /E /Y
+cd .\Server
+node server.js
+echo Server started
+pause
