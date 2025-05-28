97f8d50659fd3ba34221b5483b8c0b17ead5581d
masterccooddee <arthurhmy@gmail.com>
2025-05-28 19:09:46 +0800

    music player can see

--- src/components/MusicPlayer/MusicPlayer.jsx
+++ src/components/MusicPlayer/MusicPlayer.jsx
@@ -1,31 +1,36 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import useMusicPlayer from './music_implement.jsx';
 import { useLocation } from 'react-router-dom';
 import './index.css';
 
 const MusicPlayer = () => {
-  // const audioRef = useRef(new Audio());
-  // new add
-  const location = useLocation();
-      const musicData = location.state || {};
-      console.log('musicData:', musicData);
-      let Musicid = '';
-      if (musicData.type !== "music") { // 處理series
-          Musicid = String(musicData.from_music_id) + '-' + String(musicData.id);
-      }
-      else{
-          Musicid = String(musicData.id)
-      }
-      console.log('musicID:', Musicid);
-  //
-  // const { render } = useMusicPlayer(audioRef);
-  const { render } = useMusicPlayer(Musicid);
+    // const audioRef = useRef(new Audio());
+    // new add
+    const location = useLocation();
+    const musicData = location.state || {};
+    console.log('musicData:', musicData);
+    let Musicid = '';
+    if (musicData.type !== "music") { // 處理series
+        Musicid = String(musicData.from_music_id) + '-' + String(musicData.id);
+    }
+    else {
+        Musicid = String(musicData.id)
+    }
+    console.log('musicID:', Musicid);
 
-  return (
-    <div className="container">
-      {render()}
-    </div>
-  );
+    
+
+    
+    //
+    // const { render } = useMusicPlayer(audioRef);
+    const { render } = useMusicPlayer(Musicid);
+
+    return (
+        <div className="container">
+            {render()}
+            <p>hello</p>
+        </div>
+    );
 };
 
 export default MusicPlayer;
