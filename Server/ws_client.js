985de953a40a1b6c5f985e6e8cb7dd0837791726
masterccooddee <arthurhmy@gmail.com>
2025-06-06 21:20:57 +0800

    重構 ws_client.js，清理未完成影片的顯示邏輯

--- Server/ws_client.js
+++ Server/ws_client.js
@@ -1,8 +1,18 @@
 import  { WebSocket } from 'ws';
+import { loading } from './loading.js';
+import process from 'node:process';
 
 const ws = new WebSocket('ws://localhost:8080');
-
+process.stdout.write('\x1b[?25l');
 ws.onmessage = (event) => {
     const data = JSON.parse(event.data);
-    console.log('Received data:', data);
+    const notcompletedVideos = data.filter(item => !(item.finished));
+    console.clear();
+    notcompletedVideos.forEach(item => {
+        
+        console.log(item.name);
+        loading(item.percent, 100, Date.now() - item.time * 1000);
+        process.stdout.write('\n');
+    });
+
 };
