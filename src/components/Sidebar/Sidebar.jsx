31e487b780719fd8fd27d4de49dabecfa0d223ec
ddww0505 <wangys0505@gmail.com>
2025-06-10 17:43:11 +0800

    remove: 移除用不到的介面

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,11 +4,11 @@ import styles from './Sidebar.module.css';
 const navItems = [
   { icon: 'home.svg', text: '首頁', to: '/' },
   { icon: 'convert.svg', text: '轉換進度', to: '/convert' },
-  { icon: 'tag.svg', text: '標籤', to: '/tag' },
-  { icon: 'thumb_up.svg', text: '我的收藏', to: '/favorites' },
-  { icon: 'upload.svg', text: '上傳資源', to: '/upload' },
-  { icon: 'user.svg', text: '個人中心', to: '/profile' },
-  { icon: 'login.svg', text: '登入 / 註冊', to: '/login' }
+  // { icon: 'tag.svg', text: '標籤', to: '/tag' },
+  // { icon: 'thumb_up.svg', text: '我的收藏', to: '/favorites' },
+  // { icon: 'upload.svg', text: '上傳資源', to: '/upload' },
+  // { icon: 'user.svg', text: '個人中心', to: '/profile' },
+  // { icon: 'login.svg', text: '登入 / 註冊', to: '/login' }
 ];
 
 function Sidebar({ isDarkMode, isCollapsed, isSidebarOpen, onToggleSidebar, onToggleDarkMode, onCloseSidebar }) {
